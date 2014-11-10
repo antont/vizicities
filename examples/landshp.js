66c86912ec1dbeb77f3cd0f2d1e3bc5dd43abfa2
Timo Asumaniemi <timo.asumaniemi@gmail.com>
2014-11-10 16:11:17 +0200

    working oulu land via blender now, shp parsing not used.

--- examples/landshp.js
+++ examples/landshp.js
@@ -16,27 +16,51 @@
 		
 		//var landGeom = new THREE.PlaneGeometry(40000, 40000, 4, 4);
 		
+		/* using SHP for the Oulu export from QGIS was borked (broken geom) so going via Blender & Three.json now
 		parsedShp = null;
-		SHPParser.load('shp.js/110m_land.shp', function(res) {
+		SHPParser.load('shp.js/qgis-hietasaari.shp', function(res) {
+		//SHPParser.load('shp.js/oulu_land-polygons-split-3857.shp', function(res) {
           console.log('ok', res);
           parsedShp = res;
           var loader = new THREE.SHPLoader();
-          /*var m = loader.createModel(parsedShp, false);
-          m.scale.set(1.01, 1.01, 1.01);
-          console.log('created model', m);*/
+          //var m = loader.createModel(parsedShp, false);
+          //m.scale.set(1.01, 1.01, 1.01);
+          //console.log('created model', m);
           var compressed = loader.compress(parsedShp);
           land = loader.loadCompressed(compressed, false);
-          /*s.add(new THREE.Mesh(
-            new THREE.SphereGeometry(88,64, 64),
-            new THREE.MeshBasicMaterial({color: 0x0000FF, opacity: 0.3, depthWrite:false})
-          ));*/
+          //s.add(new THREE.Mesh(
+          //  new THREE.SphereGeometry(88,64, 64),
+          //  new THREE.MeshBasicMaterial({color: 0x0000FF, opacity: 0.3, depthWrite:false})
+          //));
           //s.add(m);
 		  
-		  land.scale.set(100, 100, 1);
+		  //land.scale.set(1, 1, 1);
 		  land.position.y = -0.2;
 		  land.rotation.x = - 90 * Math.PI / 180;
 		  city.webgl.scene.scene.add(land);
 		});
+		*/
+		
+		var loader = new THREE.JSONLoader();
+		loader.load( 'land-oulu.json', function ( geometry, materials ) {
+					//color
+					/*var material = materials[ 0 ];
+					material.color.setHex( 0xffaaaa );
+					material.ambient.setHex( 0x222222 );
+					*/
+
+					var faceMaterial = new THREE.MeshFaceMaterial( materials );
+					var mat = new THREE.MeshBasicMaterial({color: 0xFF0000}); //, opacity: 1, depthWrite: false})
+					var land = new THREE.Mesh(geometry, mat) 
+					
+					land.position.y = -0.2;
+					//land.rotation.x = - 90 * Math.PI / 180;
+					//land.scale.set(1, 1, 1);
+
+					city.webgl.scene.scene.add(land);
+					console.log("LAND ADDED");
+					THREE.land = land; //for console debug
+		});		
 		
 		/*
 		var landMat = new THREE.MeshBasicMaterial({color: 0x08f808});
